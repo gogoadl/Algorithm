f0ecb340df8fdaa6bc6c7c4a18c6301fb53e796e
gogoadl <49335446+gogoadl@users.noreply.github.com>
2020-06-23 21:40:07 +0900

    알고리즘 문제 공부중

---
+++ src/com/company/StockPrice.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class StockPrice {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        StockPrice stockPrice = new StockPrice();
+        stockPrice.solution(new int[]{1,2,3,2,3});
+    }
+}
--- src/com/company/TargetNumber.java
+++ src/com/company/TargetNumber.java
@@ -3,10 +3,18 @@ package com.company;
 public class TargetNumber {
     public int solution(int[] numbers, int target) {
         int answer = 0;
-
+        answer = dfs(numbers, 0, 0, target);
         return answer;
     }
-
+    private int dfs(int[] numbers, int node, int sum, int target){
+        if(node == numbers.length){ // 노드의 차수가 5
+            if(sum==target) //
+                return 1;
+            return 0;
+        }
+        return dfs(numbers, node+1, sum + numbers[node], target)
+                + dfs(numbers, node+1, sum - numbers[node], target);
+    }
     public static void main(String[] args) {
         TargetNumber targetNumber = new TargetNumber();
         targetNumber.solution(new int[]{1,1,1,1,1}, 3);
