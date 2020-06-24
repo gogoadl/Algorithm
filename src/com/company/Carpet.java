9d42be73da7f821322e81839e6199c14b6ce756d
gogoadl <49335446+gogoadl@users.noreply.github.com>
2020-06-24 21:48:59 +0900

    카펫 성공

--- src/com/company/Carpet.java
+++ src/com/company/Carpet.java
@@ -5,30 +5,22 @@ import java.util.ArrayList;
 public class Carpet {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        int sum = brown + yellow;
 
-        for(int i = 2; i < sum; i++)
+        for(int i = 1; i <= yellow; i++)
         {
-            if(sum % i == 0)
+            if(yellow % i == 0)
             {
-                arrayList.add(i);
+                if(2 * i + (2 * yellow / i) == brown - 4)
+                {
+                    answer[0] = i + 2;
+                    answer[1] = yellow / i + 2;
+                }
             }
-        }
-        int temp = (arrayList.size() / 2);
-        if(arrayList.size() % 2 == 0)
-        {
-            answer[0] = arrayList.get(temp);
-            answer[1] = arrayList.get(temp - 1);
-        }else
-        {
-            answer[0] = arrayList.get(temp);
-            answer[1] = arrayList.get(temp);
         }
         return answer;
     }
     public static void main(String[] args) {
         Carpet carpet = new Carpet();
-        carpet.solution(14,4);
+        carpet.solution(10,2);
     }
 }
