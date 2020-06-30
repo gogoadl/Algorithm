3c4c2b341c9a1e3952828032ea7f411c2e16803e
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2020-06-30 18:25:37 +0900

    더 맵게 풀이

--- src/com/company/MoreSpicy.java
+++ src/com/company/MoreSpicy.java
@@ -1,35 +1,30 @@
 package com.company;
 
-
 import java.util.PriorityQueue;
 
 public class MoreSpicy {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-
         for (int num:scoville)
         { priorityQueue.add(num); }
-
-        while(true)
+        while(!priorityQueue.isEmpty())
         {
-
-            int a = priorityQueue.poll();
-            int b = priorityQueue.poll();
-            priorityQueue.add(a + (b * 2));
-            answer++;
-            if(priorityQueue.isEmpty())
-                return -1;
-                break;
+            try {
+                int a = priorityQueue.poll();
+                int b = priorityQueue.poll();
+                priorityQueue.add(a + (b * 2));
+                answer++;
+                if(priorityQueue.peek() > K)
+                    break;
+            }catch (Exception e)
+            { return -1; }
         }
-//        for (int i = 0; i <arrayList.size() ; i++) {
-//            System.out.println(arrayList.get(i));
-//        }
         return answer;
     }
 
     public static void main(String[] args) {
         MoreSpicy moreSpicy = new MoreSpicy();
-        moreSpicy.solution(new int[]{1,2,3,9,10,12},7);
+        moreSpicy.solution(new int[]{0,0,0,0},7);
     }
 }
--- src/com/company/Practice22.java
+++ src/com/company/Practice22.java
@@ -1,4 +1,28 @@
 package com.company;
 
-public class Practice22 {
+public class Practice22 { // 숫자의 표현
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            int sum = 0;
+            for(int j = i; j < n; j++)
+            {
+                sum += j;
+                if(sum == n)
+                {
+                    answer++;
+                    break;
+                }else if (sum > n)
+                    break;
+            }
+
+        }
+        return answer + 1;
+    }
+
+    public static void main(String[] args) {
+        Practice22 practice22 = new Practice22();
+        practice22.solution(15);
+    }
 }
