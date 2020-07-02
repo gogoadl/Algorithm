6fc986b2829bb5529842b29df066e4e2c8c42008
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2020-07-02 18:02:38 +0900

    땅따먹기 DP 해보는 중

---
+++ src/com/company/Practice23.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Practice23 { // 땅따먹기
+    int solution(int[][] land) {
+        int answer = 0;
+
+        for(int i = 0; i < land.length; i++)
+            for(int j = 0; j < land[i].length; j++)
+            {
+
+            }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Practice23 practice23 = new Practice23();
+        practice23.solution(new int[][]{{5,6,7,8},{5,6,7,8},{4,3,2,1}});
+    }
+}
