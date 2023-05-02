6614e3d5d9d99df8b508744773eef850832111af
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-05-02 18:33:32 +0900

    Create BaekJoon2751.java

---
+++ src/com/company/BaekJoon2751.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class BaekJoon2751 {
+    
+    public static void main(String[] args) throws IOException {
+        LinkedList<Integer> set = new LinkedList();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String count = br.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < Integer.parseInt(count); i++) {
+            int value = Integer.parseInt(br.readLine());
+            set.add(value);
+        }
+        Collections.sort(set);
+        for (int value: set) {
+            stringBuilder.append(value + "\n");
+        }
+        System.out.println(stringBuilder);
+    }
+}
