a59824738016c3555365b06e2bdddc0686fa94d0
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-05-03 18:51:49 +0900

    Create SWEA2072.java

---
+++ src/com/company/SWEA2072.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SWEA2072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCaseCount = sc.nextInt();
+        int plusCount = 10;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= testCaseCount; i++) {
+            int answer = 0;
+            for (int j = 0; j < plusCount; j++) {
+                int value = sc.nextInt();
+                if (value % 2 != 0) {
+                    answer += value;
+                }
+            }
+            sb.append(String.format("#%d %d\n", i, answer));
+        }
+        System.out.println(sb);
+    }
+}
