74fc2892bef7c6578aee45d51e6d2c9a860383a6
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-01 22:55:59 +0900

    Create BaekJoon10171.java

---
+++ src/com/company/baekjoon/BaekJoon10171.java
@@ -0,0 +1,10 @@
+package com.company.baekjoon;
+
+public class BaekJoon10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
