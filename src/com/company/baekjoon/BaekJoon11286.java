557b909dd435bcc8b588367cec342aa2d42d07c4
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-09 16:16:09 +0900

    Create BaekJoon11286.java

---
+++ src/com/company/baekjoon/BaekJoon11286.java
@@ -0,0 +1,41 @@
+package com.company.baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BaekJoon11286 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return Math.abs(o1) - Math.abs(o2);
+                }
+            }
+        });
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (!queue.isEmpty()) {
+                    int min = queue.poll();
+                    sb.append(min).append("\n");
+                } else {
+                    sb.append("0\n");
+                }
+            } else {
+                queue.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
