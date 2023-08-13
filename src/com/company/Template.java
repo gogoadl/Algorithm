e735823cb231355a0e23fe80505b13b1d8732fe7
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-13 23:36:34 +0900

    Create Template.java

---
+++ src/com/company/Template.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Template {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+    }
+}
