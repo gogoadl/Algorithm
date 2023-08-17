f9ce87d82fcb8fbab901a4638f09db6d7a76ec20
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-17 15:50:13 +0900

    Add files via upload

---
+++ src/com/company/baekjoon/BaekJoon1026.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon1026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer += A[i] * B[N-i-1];
+        }
+        System.out.println(answer);
+    }
+}
