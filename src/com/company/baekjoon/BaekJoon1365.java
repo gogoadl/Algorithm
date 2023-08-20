0055a5e97967691b807762562bed20df3d474c9c
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-20 19:19:50 +0900

    Update BaekJoon1365.java

--- src/com/company/baekjoon/BaekJoon1365.java
+++ src/com/company/baekjoon/BaekJoon1365.java
@@ -34,7 +34,7 @@ public class BaekJoon1365 {
         dp = new int[array.length+1];
         int len = 0;
         int idx = 0;
-        for(int i=0; i<array.length-1; i++) {
+        for(int i=0; i<array.length; i++) {
             if(array[i] > dp[len]) {
                 len +=1;
                 dp[len] = array[i];
