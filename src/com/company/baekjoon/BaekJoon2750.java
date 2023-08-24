7dc556790de9c21fef08f4ff775c676afaf9bcdf
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-24 22:17:23 +0900

    Create BaekJoon2750.java

---
+++ src/com/company/baekjoon/BaekJoon2750.java
@@ -0,0 +1,27 @@
+package com.company.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int value: arr) {
+            sb.append(value).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
