6bffaad33b9a13133e33f5bfa47aa06cb2e33d1e
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-24 21:54:35 +0900

    Create BaekJoon1920.java

---
+++ src/com/company/baekjoon/BaekJoon1920.java
@@ -0,0 +1,53 @@
+package com.company.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon1920 {
+    public static boolean find(int[] array, int target) {
+        int start = 0;
+        int end = array.length;
+        int mid;
+        while (start<end) {
+            mid = (start + end) / 2;
+            if (array[mid] == target) {
+                return true;
+            } else if (array[mid] > target){
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            if (find(arr, value)) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
