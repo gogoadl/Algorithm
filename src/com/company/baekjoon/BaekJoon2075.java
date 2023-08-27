53b39ef9669982ad7b9a8057b3257b98c229d214
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-27 20:54:46 +0900

    Create BaekJoon2075.java

---
+++ src/com/company/baekjoon/BaekJoon2075.java
@@ -0,0 +1,29 @@
+package com.company.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BaekJoon2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for (int i = 0; i < N-1; i++) {
+            pq.poll();
+        }
+        System.out.println(pq.poll());
+    }
+}
