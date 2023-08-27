30a87887b8e16b58d37176dfbf7af02bb68f36db
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-08-27 22:14:37 +0900

    29트만에 풀었다

--- src/com/company/baekjoon/BaekJoon2733.java
+++ src/com/company/baekjoon/BaekJoon2733.java
@@ -1,87 +1,93 @@
 package com.company.baekjoon;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class BaekJoon2733 {
-    public static void main(String[] args) throws IOException {
+    public static int ptr;
+    public static char[] arr;
+    public static String code;
+    public static StringBuilder res = new StringBuilder();
+    public static Map<Integer, Integer> indexMap = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
-        int testCaseCount = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            ptr = 0;
+            indexMap.clear();
+            arr = new char[32768];
+            boolean isBreak = false;
+            res.append(String.format("PROGRAM #%d:\n", i));
 
-        for (int i = 1; i <= testCaseCount; i++) {
-            char[] array = new char[32768];
-            int index = 0;
-            HashMap<Integer, Integer> map = new HashMap<>();
-            Stack<Integer> stack = new Stack<>();
-            StringBuilder command = new StringBuilder();
-            StringBuilder result = new StringBuilder();
-            result.append(String.format("Program #%d:\n", i));
+            StringBuilder sb = new StringBuilder();
             while (true) {
-                String input = br.readLine();
-                input = input.replaceAll(" ",""); // 공백제거
+                String line = br.readLine();
+                if ("end".equals(line)) {
+                    break;
+                }
 
-                if (input.equals("end")) break;
-
-                if (input.contains("%")) {
-                    command.append(input, 0, input.indexOf("%"));
-                } else
-                    command.append(input);
+                if (line.contains("%")) {
+                    line = line.substring(0, line.indexOf("%"));
+                }
+                sb.append(line);
             }
-            String s = command.toString();
-            for (int j = 0; j < s.length(); j++) { // 괄호가 있다면 스택에 추가
-                if (s.charAt(j) == '[') {
-                    stack.add(j);
-                } else if (s.charAt(j) == ']') {
-                    if (stack.isEmpty()) {// 스택이 비었다면 컴파일 에러 (짝 안맞음)
-                        result.append("COMPILE ERROR");
+            code = sb.toString();
+
+            Stack<Integer> stk = new Stack<>();
+
+            for (int j = 0; j < code.length(); j++) {
+                char ch = code.charAt(j);
+                if (ch == '[') {
+                    stk.push(j);
+                } else if (ch == ']') {
+                    if (stk.isEmpty()) {
+                        isBreak = true;
                         break;
                     } else {
-                        int left = stack.pop();
-                        map.put(left, j);
-                        map.put(j, left);
+                        int idx = stk.pop();
+                        indexMap.put(j, idx);
+                        indexMap.put(idx, j);
                     }
                 }
             }
-            if (!stack.isEmpty()) { // 스택이 비어있지 않으면 컴파일 에러 (짝 안맞음)
-                result.append("COMPILE ERROR");
-                sb.append(result).append("\n");
-                continue;
-            }
-            char[] chars = s.toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                if (chars[j] == '>') {
-                    if (index == 32767) index = 0;
-                    else index++;
-                } else if (chars[j] == '<') {
-                    if (index == 0) index = 32767;
-                    else index--;
-                } else if (chars[j] == '+') {
-                    if (array[index] == 255) array[index] = 0;
-                    array[index]++;
-                } else if (chars[j] == '-') {
-                    if (array[index] == 0) array[index] = 255;
-                    array[index]--;
-                } else if (chars[j] == '.') {
-                    result.append(array[index]);
-                } else if (chars[j] == '[') {
-                    if (array[index] == 0) {
-                        j = map.get(j);
-                        j--;
-                    }
-                } else if (chars[j] == ']') {
-                    if (array[index] != 0) {
-                        j = map.get(j);
-                        j--;
+
+            if (stk.isEmpty() && !isBreak) {
+                for (int j = 0; j < code.length(); j++) {
+                    char c = code.charAt(j);
+                    if (c == '>') {
+                        if (ptr == 32767) ptr = 0;
+                        else ptr++;
+                    } else if (c == '<') {
+                        if (ptr == 0) ptr = 32767;
+                        else ptr--;
+                    } else if (c == '+') {
+                        if (arr[ptr] == 255) arr[ptr] = 0;
+                        arr[ptr]++;
+                    } else if (c == '-') {
+                        if (arr[ptr] == 0) arr[ptr] = 255;
+                        arr[ptr]--;
+                    } else if (c == '.') {
+                        res.append(arr[ptr]);
+                    } else if (c == '[') {
+                        if (arr[ptr] == 0) {
+                            j = indexMap.get(j);
+                        }
+                    } else if (c == ']') {
+                        if (arr[ptr] != 0) {
+                            j = indexMap.get(j) - 1;
+                        }
                     }
                 }
+            } else {
+                res.append("COMPILE ERROR");
             }
-            sb.append(result).append("\n");
+            res.append("\n");
         }
-        System.out.println(sb);
+        System.out.println(res);
     }
 }
