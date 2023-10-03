486fda29b4e01224b91a68f89782e0fb68ad8497
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-10-03 21:19:48 +0900

    [Silver I] Title: 회의실 배정, Time: 556 ms, Memory: 57348 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int start = Integer.parseInt(input[0]);
+            int end = Integer.parseInt(input[1]);
+            arr[i][0] = start;
+            arr[i][1] = end;
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+        int t = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] >= t) {
+                t = arr[i][1];
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
