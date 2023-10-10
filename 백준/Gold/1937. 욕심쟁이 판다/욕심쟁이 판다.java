01d7a42ff8bf26d9bf942e1c9d3d5c4964f3df07
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-10-10 15:51:18 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 468 ms, Memory: 40788 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,41 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
-    static int n,m;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] arr;
+    static int[] dx = new int[]{0,0,-1,1};
+    static int[] dy = new int[]{-1,1,0,0};
+    static int n;
     static int[][] dp;
-    static int max = 0;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-        arr = new int[n][n];
+    static int[][] graph;
+    static int maxSize = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        dp = new int[n][n];
+        graph = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                arr[i][j] = scan.nextInt();
+                maxSize = Math.max(maxSize, updateMaxSize(i,j));
             }
         }
-        dp = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                max = Math.max(dfs(i,j), max);
-            }
-        }
-        System.out.println(max);
+        System.out.println(maxSize);
     }
-    public static int dfs(int x, int y) {
-        if (dp[x][y] != 0) {
-            return dp[x][y];
-        }
-        dp[x][y] = 1;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx >= 0 && ny >= 0 && nx < n && ny < n){
-                if(arr[nx][ny] > arr[x][y])
-                    dp[x][y] = Math.max(dp[x][y], dfs(nx,ny) + 1);
+    static int updateMaxSize(int i, int j) {
+        if (dp[i][j] != 0) return dp[i][j];
+
+        dp[i][j] = 1;
+        for (int k = 0; k < 4; k++) {
+            int cy = i + dy[k];
+            int cx = j + dx[k];
+
+            if (cy < 0 || cx < 0 || cy >= n || cx >= n) continue;
+            if (graph[cy][cx] > graph[i][j]) {
+                dp[i][j] = Math.max(dp[i][j], updateMaxSize(cy,cx)+1);
             }
         }
-        return dp[x][y];
+
+        return dp[i][j];
     }
+
 }
