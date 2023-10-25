222affaa0baeb79865fcd7943d09d3e260edc6f4
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-10-25 22:48:52 +0900

    [Gold IV] Title: 저울, Time: 116 ms, Memory: 13432 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            arr[a][b] = 1;
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1)
+                        arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cnt = 0;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (arr[i][j] == 0 && arr[j][i] == 0) cnt++;
+            }
+            sb.append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
