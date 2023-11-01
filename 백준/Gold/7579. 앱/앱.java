3145facf1bc95f99a667cf7c8938f5c8dc462d67
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-11-02 08:25:27 +0900

    [Gold III] Title: 앱, Time: 116 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -10,28 +10,29 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
+        int allCost = 0;
         int[] apps = new int[n];
         int[] memories = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            apps[i] = Integer.parseInt(st.nextToken());
+            memories[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            memories[i] = Integer.parseInt(st.nextToken());
+            apps[i] = Integer.parseInt(st.nextToken());
+            allCost += apps[i];
         }
         int max = Integer.MAX_VALUE;
 
-        int[][] dp = new int[n][100001];
+        int[][] dp = new int[n][allCost+1];
 
         for (int i = 0; i < n; i++) {
-            int memory = apps[i];
-            int app = memories[i];
+            int memory = memories[i];
+            int app = apps[i];
 
-            for (int j = 0; j < 100000; j++) {
+            for (int j = 0; j <= allCost; j++) {
                 if (i == 0) {
                     if (j >= app)
                         dp[i][j] = memory;
