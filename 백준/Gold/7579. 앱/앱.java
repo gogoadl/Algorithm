1b91caead4aeafa16682ad0f31fbfa58896be006
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-11-02 08:15:05 +0900

    [Gold III] Title: 앱, Time: 204 ms, Memory: 51456 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] apps = new int[n];
+        int[] memories = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            apps[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            memories[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = Integer.MAX_VALUE;
+
+        int[][] dp = new int[n][100001];
+
+        for (int i = 0; i < n; i++) {
+            int memory = apps[i];
+            int app = memories[i];
+
+            for (int j = 0; j < 100000; j++) {
+                if (i == 0) {
+                    if (j >= app)
+                        dp[i][j] = memory;
+                } else {
+                    if (j >= app) {
+                        dp[i][j] = Math.max(dp[i-1][j-app] + memory, dp[i-1][j]);
+                    } else {
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+                if (dp[i][j] >= m) {
+                    max = Math.min(max, j);
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
