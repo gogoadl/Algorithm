090598d5a768361e0cfc3269c69e65254400d108
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2024-02-21 10:08:18 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static long[] dp = new long[1001];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 3;
+		for (int i = 4; i <= n; i++) {
+			dp[i] = (dp[i-1] % 10007) + (dp[i-2] % 10007) % 10007;
+		}
+
+		System.out.println(dp[n] % 10007);
+	}
+}
