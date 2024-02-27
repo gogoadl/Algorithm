1bf90f516a0f3f7dc474b62ddd11c6f4d4c857e0
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2024-02-27 10:27:49 +0900

    [Silver I] Title: 행렬, Time: 80 ms, Memory: 11872 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int m;
+	static int[][] arr;
+	static int[][] target;
+	static int count;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[n][m];
+		target = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = str.charAt(j)-48;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			for (int j = 0; j < m; j++) {
+				target[i][j] = str.charAt(j)-48;
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (arr[i][j] != target[i][j] && i+3 <= n && j+3 <= m) {
+					flip(i, j);
+					count++;
+				}
+			}
+		}
+
+		boolean isTranslated = true;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (arr[i][j] != target[i][j]) {
+					System.out.println(-1);
+					return;
+				}
+			}
+		}
+
+		System.out.println(count);
+	}
+	public static void flip(int i, int j) {
+		for (int k = i; k < i + 3; k++) {
+			for (int l = j; l < j + 3; l++) {
+				arr[k][l] = arr[k][l] == 1 ? 0 : 1;
+			}
+		}
+	}
+}
