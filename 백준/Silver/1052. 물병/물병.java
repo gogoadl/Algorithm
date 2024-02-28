20ffe4d4266029d38ff967bab1e83e498fabf4cc
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2024-02-28 10:26:21 +0900

    [Silver I] Title: 물병, Time: 80 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/1052. 물병/물병.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int val;
+		while (k > 1) {
+			val = 0;
+			while (n > Math.pow(2, val)) {
+				val++;
+			}
+			n -= Math.pow(2, val-1);
+			if (n == 0) {
+				System.out.println(0);
+				return;
+			}
+			k--;
+		}
+
+		val = 0;
+
+		while (n > Math.pow(2, val)) {
+			val++;
+		}
+
+		System.out.println((int)Math.pow(2, val)-n);
+
+	}
+}
