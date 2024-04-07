374d8eaf1dfb8f7b74a54f46114c0b7c4bef3777
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2024-04-08 08:49:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -1,3 +1,11 @@
 class Solution {
-fun solution(numbers: IntArray): Int  = 45 - numbers.sum()
+    fun solution(numbers: IntArray): Int {
+        var answer: Int = -1
+        var sum = 0
+        for(i in 0..numbers.size-1){
+            sum += numbers[i]
+        }
+        answer = 45 - sum
+        return answer
+    }
 }
