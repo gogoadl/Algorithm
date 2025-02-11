aa83c196c8977e3a56430874f5f1aa4c5267a0a6
HyeonWoo Park <gogoadl@naver.com>
2025-02-11 23:32:17 +0900

    [Silver V] Title: 돌 게임, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        if (N % 2 == 1)
+            System.out.println("SK");
+        else
+            System.out.println("CY");
+    }
+}
